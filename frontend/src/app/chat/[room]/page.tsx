d2dfa7f8ab5ff0250d63e5ad86e69ef6adc0c52c
Diego Moura <diegosouza20049@hotmail.com>
2024-10-18 20:59:04 -0300

    usa nome do usuario vindo do contexto para estilizar

--- frontend/src/app/chat/[room]/page.tsx
+++ frontend/src/app/chat/[room]/page.tsx
@@ -31,30 +31,28 @@ export default function Page() {
     }
 
     let newMessage: Message = {
-      sender: "Diego Moura",
+      sender: user as string,
       content: messageContent,
     };
 
     socket.emit("message", { room, message: newMessage });
 
     setMessageContent("");
-  };
-
-  const self = false;
+  };  
 
   return (
     <main className="flex flex-col justify-between bg-[#0a0a0a] min-h-96">
-      <div className="flex flex-col w-full text-black ">
+      <div className="flex flex-col w-full p-1 text-black">
         {messages.map((message, index) => (
           <div
             key={index}
             className={`flex flex-row w-full ${
-              self ? "justify-end" : "justify-start"
+              message.sender === user ? "justify-end" : "justify-start"
             }`}
           >
             <div
               className={`mt-1 ${
-                self ? "bg-[#008069]" : "bg-gray-600"
+                message.sender === user ? "bg-[#008069]" : "bg-gray-600"
               } rounded-lg w-fit`}
             >
               <span className="p-2 font-bold text-green-300">~ {message.sender}</span>
