da459f7338f069f4d3f64f26fab331b7560afff5
Diego Moura <diegosouza20049@hotmail.com>
2024-10-31 19:16:16 -0300

    remove comment

--- backend/src/models/DB.ts
+++ backend/src/models/DB.ts
@@ -1,10 +1,7 @@
 import sql  from 'mssql'
 import dotenv from 'dotenv'
 
-
-
 dotenv.config()
-
 
 const sqlConfig = {
     user: process.env.DB_USER,
@@ -24,22 +21,9 @@ const sqlConfig = {
 
   function query(query: string) {
     // sql.connect() will return the existing global pool if it exists or create a new one if it doesn't
-    return sql.connect(sqlConfig as any).then( (pool) => {
+    return sql.connect(sqlConfig as any).then((pool) => {
       return  pool.query(query)
     })
   }
-
-
-
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0,
-
-// })
 
 export { query }
