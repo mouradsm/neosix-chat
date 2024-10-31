cfa19f79feedcc9acdfeea0be01741a4e5d9509b
Diego Moura <diegosouza20049@hotmail.com>
2024-10-31 18:19:47 -0300

    add mssql connection

--- backend/src/models/DB.ts
+++ backend/src/models/DB.ts
@@ -1,17 +1,45 @@
-import mysql from 'mysql2/promise'
+import sql  from 'mssql'
 import dotenv from 'dotenv'
+
+
 
 dotenv.config()
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
+
+const sqlConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    server: process.env.DB_HOST,
+    pool: {
+      max: 10,
+      min: 0,
+      idleTimeoutMillis: 30000
+    },
+    options: {
+      encrypt: false, // for azure
+      trustServerCertificate: false // change to true for local dev / self-signed certs
+    }
+  }
 
-})
+  function query(query: string) {
+    // sql.connect() will return the existing global pool if it exists or create a new one if it doesn't
+    return sql.connect(sqlConfig as any).then( (pool) => {
+      return  pool.query(query)
+    })
+  }
 
-export default pool
+
+
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+
+// })
+
+export { query }
